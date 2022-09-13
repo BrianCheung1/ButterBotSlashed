b471de3fa5b2794dfd07d9f038eb2ba3b57c2adc
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2022-09-12 20:00:00 -0400

    added moderation commands

---
+++ cogs/moderation.py
@@ -0,0 +1,71 @@
+from discord import app_commands
+from discord.ext import commands
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from typing import Optional
+import discord
+import os
+import random
+
+
+load_dotenv()
+list_of_guilds = os.getenv("GUILDS").split(",")
+MY_GUILDS = [discord.Object(id=int(guild)) for guild in list_of_guilds]
+
+
+class Moderation(commands.Cog):
+    """Moderation Features"""
+
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @app_commands.command(
+        name="purge",
+        description="Deletes messages - 100 messages max, less than 14 day old messages",
+    )
+    @app_commands.describe(
+        amount="amount of messages to purge default 5, min - 1, max - 100"
+    )
+    async def purge(
+        self,
+        interaction: discord.Interaction,
+        amount: Optional[app_commands.Range[int, 1, 100]] = 5,
+    ):
+        await interaction.response.defer()
+        messages = [
+            message async for message in interaction.channel.history(limit=amount + 1)
+        ]
+        if len(messages) >= 1:
+            messages.pop(0)
+        await interaction.channel.delete_messages(messages)
+        await interaction.followup.send(f"{amount} messages deleted")
+
+    @app_commands.command(
+        name="role",
+        description="Give a member a role or delete the role if they have it",
+    )
+    @app_commands.describe(
+        member="member you want to give a role to",
+        role="role you want to give to a member",
+    )
+    async def role(
+        self,
+        interaction: discord.Interaction,
+        member: discord.Member,
+        role: discord.Role,
+    ):
+        if role in member.roles:
+            await member.remove_roles(role)
+            await interaction.response.send_message(
+                f"{role.mention} role removed from {member.mention}"
+            )
+        else:
+            await member.add_roles(role)
+            await interaction.response.send_message(
+                f"{role.mention} role given to {member.mention}"
+            )
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Moderation(bot), guilds=MY_GUILDS)
+    print("Moderation is Loaded")
